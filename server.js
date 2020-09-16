ea8156d29610146bb6f373af1dc1b037fba6fa42
samerk <samer.krayem@netcompany.com>
2020-09-16 17:14:49 +0100

    server.js update

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-//Install express server
+//Install express server    
 const express = require('express');
-const path = require('path');
+const path = require('path');   
 
-const app = express();
+// var cors = require('cors');
+const app = express();   
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
 });
 
-// Start the app by listening on the default Heroku port
+  
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+});  
+
+
 app.listen(process.env.PORT || 8080);
